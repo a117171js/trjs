0104df21dbe3267fd0df1d82040acd0fa7c97f3d
a117171js <a117171@hotmail.com>
2025-08-26 13:43:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -179,10 +179,10 @@ const worker_default = {
             // return new Response('Not found', { status: 404 });
             // For any other path, reverse proxy to 'ramdom website' and return the original response, caching it in the process
             if (cn_hostnames.includes('')) {
-            return new Response(JSON.stringify(request.cf, null, 4), {
+            return new Response(await nginx(), {
               status: 200,
               headers: {
-                "Content-Type": "application/json;charset=utf-8",
+                "Content-Type": "text/html;charset=utf-8",
               },
             });
             }
@@ -2662,3 +2662,33 @@ return `{
     }
   }
 })();
+async function nginx() {
+    const text = `
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<title>Welcome to nginx!</title>
+	<style>
+		body {
+			width: 35em;
+			margin: 0 auto;
+			font-family: Tahoma, Verdana, Arial, sans-serif;
+		}
+	</style>
+	</head>
+	<body>
+	<h1>Welcome to nginx!</h1>
+	<p>If you see this page, the nginx web server is successfully installed and
+	working. Further configuration is required.</p>
+	
+	<p>For online documentation and support please refer to
+	<a href="http://nginx.org/">nginx.org</a>.<br/>
+	Commercial support is available at
+	<a href="http://nginx.com/">nginx.com</a>.</p>
+	
+	<p><em>Thank you for using nginx.</em></p>
+	</body>
+	</html>
+	`
+    return text;
+}
